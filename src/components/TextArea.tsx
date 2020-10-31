4c5938658fe5c43c2434e9dabefd0bdb0e3d7039
seimori <issei.morimoto@gmail.com>
2020-10-31 22:45:46 +0100

    feat: Start of implementation of Draft.js

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
 
 const TextArea = () => {
-  return <div>Text Area</div>;
+  const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
+  return (
+    <div>
+      <h1>Text Area</h1>
+      <Editor editorState={editorState} onChange={setEditorState} />
+    </div>
+  );
 };
 
 export default TextArea;
