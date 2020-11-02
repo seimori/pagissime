1d6047216041f164025d819c7319281b99448818
seimori <issei.morimoto@gmail.com>
2020-11-02 17:26:19 +0100

    feat: Testing Draft.js capabilities. Add buttons for Bold and Italic

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 const TextArea = () => {
   const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
+
+  const handleKeyCommand = (command: string, editorState: EditorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      setEditorState(newState);
+      return "handled";
+    }
+    return "not-handled";
+  };
+
+  const onBoldClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+  const onItalicClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
+  };
+
   return (
     <div>
       <h1>Text Area</h1>
-      <Editor editorState={editorState} onChange={setEditorState} />
+      <div style={{ border: "solid gray 1px" }}>
+        <button onClick={onBoldClick}>Bold</button>
+        <button onClick={onItalicClick}>Italic</button>
+        <Editor editorState={editorState} onChange={setEditorState} handleKeyCommand={handleKeyCommand} />
+      </div>
     </div>
   );
 };
